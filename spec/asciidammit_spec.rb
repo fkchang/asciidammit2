7269ae5cf12f1da6229baa222b2dd1afccc8d2f8
Forrest Chang <fchang@hedgeye.com>
2022-04-28 17:17:27 -0700

    update dependencies and spec syntax, bump version

--- spec/asciidammit_spec.rb
+++ spec/asciidammit_spec.rb
@@ -3,7 +3,7 @@ require 'asciidammit'
 describe Asciidammit do
   # keep this for handy string w/lots of symbols
   it "should demoronize the special characters provided by word" do
-    Asciidammit.demoronize( "—–‑­ ©®™§¶…‘’“”" ).should ==  "----  (c)[R][TM]SP...''\"\""
+    expect(Asciidammit.demoronize( "—–‑­ ©®™§¶…‘’“”" )).to eq  "----  (c)[R][TM]SP...''\"\""
   end
   # the MS Word provided symbols 1 at a time
   [
